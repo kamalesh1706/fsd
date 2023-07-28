5e9cf6e1b0d5540b43ed784483884a84ac606330
kamalesh0617 <126144547+kamalesh1706@users.noreply.github.com>
2023-07-28 15:29:39 +0530

    Add files via upload

---
+++ Day14 Redux/Store.js
@@ -0,0 +1,33 @@
+import { createStore } from 'redux';
+
+const UPDATE_NAME = 'UPDATE_NAME';
+
+
+export const updateName = (newName) => {
+  return {
+    type: UPDATE_NAME,
+    payload: newName,
+  };
+};
+
+const initialState = {
+  name: 'kamalesh', 
+};
+
+
+const nameReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case UPDATE_NAME:
+      return {
+        ...state,
+        name: action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+// Create and export the Redux store
+const Store = createStore(nameReducer);
+
+export default Store;
